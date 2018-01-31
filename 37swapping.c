2d27a95a4edaa86b80c30f33aad61f25835f17f5
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-01-31 16:53:43 +0530

    Create 37swapping.c

---
+++ 37swapping.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b, temp;
+printf("Enter the two numbers");
+scanf("%d%d",&a &b);
+temp=a;
+a=b;
+b=temp;
+printf("Swapping the number ");
+return 0;
+}
