2ada3af7ffdb2d4d1448b5d00658660ac2c82965
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-02-05 16:56:10 +0530

    Create 42 string .c

---
+++ 42 string .c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char str1[s1],str2[s2],i,j;
+printf("\n enter the first string");
+scanf("%s",str1);
+printf("\n enter the second string");
+scanf("%s",str2");
+int i =strlen(s1);
+int j= strlen(s2);
+if(i==j)
+{
+printf("string is equal");
+}
+if(i>j);
+{
+printf("str1  is greater");
+}
+else
+{
+printf("str2 is greater");
+}
+}
+
+
