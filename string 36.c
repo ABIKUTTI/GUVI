1bc022173704f25a082223b47e6680b47890358c
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-02-05 15:43:27 +0530

    Create string 36.c

---
+++ string 36.c
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<stdio.h>
+int main()
+{
+int i,j=0;
+char a[100];
+count<<"enter the string";
+cin>>a[100];
+for(i=0;i<100;i++)
+{
+is digit(a[i]);
+n=0;
+else
+++n;
+}
+count<<"number of numeric %d"<<j;
+getch();
+}
