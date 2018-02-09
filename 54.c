6c5a14d34c1f82d223a74cc29a67c18dcc6e3fcd
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-02-09 16:01:54 +0530

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number: ");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf(" the nearest even number is %d\n",a-2);
+}
+else
+{
+printf("the nearest even number is %d\n",a-1);
+}
+}
