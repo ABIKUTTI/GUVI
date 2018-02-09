c8958e8f76bdab2eaa5a265e22712dd069470bd1
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-02-09 16:21:29 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int 1,i,c;
+chara[20]="fancy726";
+l=strlen(a);
+for(i=1;i<=1;i++)
+{
+while(((a[i]=='a')||(a[i]=='A'))&&((a[i]=='z')||(a[i]=='Z')))
+{
+c++;
+}
+}
+if(c==1)
+{
+printf("%s is alphabet,a");
+}
+else
+{
+printf("%s is alphabet and numeric,a");
+}
+}
+
