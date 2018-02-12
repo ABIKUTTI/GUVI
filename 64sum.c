858604641d60f948295b3ca61291252a5731a6ee
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-02-12 16:31:32 +0530

    Rename 64.c to 64sum.c

---
+++ 64sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b,s;
+printf("enter the two numbers ");
+scanf("%d%d",&a,&b);
+s=a+b;
+printf("\n sum is=%d",a,b,s);
+if(s%2==0)
+{
+printf("%d is even.",s);
+}
+else
+{
+printf("%d is odd.",s);
+}
+return 0;
+}
