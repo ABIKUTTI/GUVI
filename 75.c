dd1ca771b96ff7543db19eaf32fdec388d5b4fe5
ABIKUTTI <31499358+ABIKUTTI@users.noreply.github.com>
2018-02-21 16:07:56 +0530

    Create 75.c

---
+++ 75.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+char a[10]="hello";
+int l;
+char b;
+l=strlen(a);
+b='*';
+a[1/2]=b;
+if(1%2==0)
+{
+a[(l-1)/2]=b;
+}
+printf("%s",a);
+return 0;
+}
+
